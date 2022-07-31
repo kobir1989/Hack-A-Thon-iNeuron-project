5f029cf1b6142098e043fdf1c32d0850e2c50465
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-07-31 19:04:46 +0600

    Sliding Animation added to Header

--- src/Component/Layout/DataSlider.js
+++ src/Component/Layout/DataSlider.js
@@ -8,17 +8,17 @@ const DataSlider = [
   },
   {
     id: 'A2',
-    title: 'Highest in quality, affordable in price',
-    subTitle: 'Learn from the best of industry',
+    title: "Our achivers works at top MNC's and startups",
+    subTitle: 'Learning that makes you ready for job',
     smallTitle:
-      'Digital entrepreneurs, Youtubers and content creators provide affordable courses across technologies',
+      'Over 10k+ students got placed and proved that our courses worked for them. They write their own success story and make us feel rewarded',
   },
   {
     id: 'A3',
-    title: 'Highest in quality, affordable in price',
-    subTitle: 'Learn from the best of industry',
+    title: '24 hours of learning, networking and jobs on the floor',
+    subTitle: 'Hack-A-Thon',
     smallTitle:
-      'Digital entrepreneurs, Youtubers and content creators provide affordable courses across technologsmallTitle',
+      'Our 24 hour offline coding events such as Hack-a-thons, Job-a-thons and Course-a-thons give participants an opportunity to exhibit their skills and receive prizes for completing courses, or get recruited for their skillset.',
   },
 ];
 export default DataSlider;
