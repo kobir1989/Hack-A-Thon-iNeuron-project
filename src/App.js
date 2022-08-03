28974c98bd3e694dcdc3a315f5ea61569269d3f3
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-08-04 01:36:59 +0600

    header style changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import Dashboard from './Component/StudentDashboard/Dashboard';
 import PartnerCompany from './Component/PartnerCompany/PartnerCompany';
 import OurProduct from './Component/PartnerCompany/OurProduct/OurProduct';
-import Particles from 'react-particles';
-import { loadFull } from 'tsparticles';
+
 import './App.css';
-import particlesOptions from './particles.json';
+
 import Header from './Component/Header/Header';
 import HallOfFame from './Component/HallOfFame/HallOfFame';
 import LivePrograms from './Component/LivePrograms/LivePrograms';
@@ -16,14 +15,10 @@ import Institutions from './Component/Institutions/Institutions';
 import Footer from './Component/Footer/Footer';
 
 function App() {
-  const particlesInit = useCallback((main) => {
-    loadFull(main);
-  }, []);
-
   return (
     <div>
       <Header />
-      <Particles options={particlesOptions} init={particlesInit} />
+
       <Dashboard />
       <PartnerCompany />
       <HallOfFame />
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import NavBar from '../Layout/NavBar';
+import { loadFull } from 'tsparticles';
 import './Header.css';
-
+import Particles from 'react-particles';
 import HeaderSlide from '../Layout/HeaderSlide';
+import particlesOptions from '../../particles.json';
 const Header = () => {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+  };
   return (
     <div className='header__container'>
       <NavBar />
-
+      <Particles options={particlesOptions} init={particlesInit} />
       <HeaderSlide />
     </div>
   );
